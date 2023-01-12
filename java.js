edccab8c2ce878bd48ecf1806548e07325ef3976
SathyaManikandan <psathyadev22@gmail.com>
2023-01-12 22:13:21 +0530

    Card

---
+++ java.js
@@ -0,0 +1,14 @@
+let row;
+let coloumn;
+row = window.prompt('What is the value of x')
+coloumn = window.prompt('What is the value of coloumn')
+6
+console.log(row)
+console.log(coloumn)
+
+for(let i =1; i +=1 ; i<=row){
+    for(let j=1; j+= 1 ;  j<= coloumn){
+        document.getElementById("sathya").innerHTML=j
+    }
+    
+}
